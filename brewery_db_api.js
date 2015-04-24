5d8921615ef032918e06b1875662d3a27d83cfd3
Chris Dickow <chrisdickow@gmail.com>
2015-04-23 22:22:01 -0400

    Added brewery info

--- brewery_db_api.js
+++ brewery_db_api.js
@@ -33,6 +33,7 @@ app.get('/api', function(req, res){
     api_url += '&q=' + req.query.q;
     api_url += '&type=beer';
     api_url += '&p=1'; 
+    api_url += '&withBreweries=true';
 
     
     request(api_url, function(error, response, data){
